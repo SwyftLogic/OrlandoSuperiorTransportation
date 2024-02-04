f2017437434441305757c7cabb7373b41f48e353
Umair Sarang <umair@swyftlogic.com>
2024-02-04 18:38:22 +0500

    structural changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://new-custom-template.orlandosuperiortransportation.pages.dev',
+  integrations: [sitemap()],
+  prefetch: {
+    defaultStrategy: 'hover',
+    prefetchAll: true
+  }
+});
