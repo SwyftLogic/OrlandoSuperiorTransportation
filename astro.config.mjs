e6a10802ce47ec52888992558f2a29022951aa2c
Umair Sarang <umair@swyftlogic.com>
2024-02-05 17:57:32 +0500

    tailwind and some structure udpates with form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://new-custom-template.orlandosuperiortransportation.pages.dev',
-  integrations: [sitemap()],
+  integrations: [sitemap(), tailwind()],
   prefetch: {
     defaultStrategy: 'hover',
     prefetchAll: true
