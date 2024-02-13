5a10e33f8b0413acd044610b30259e43d40f2474
Umair Sarang <umair@swyftlogic.com>
2024-02-14 01:40:09 +0500

    gtag added with partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,30 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://new-custom-template.orlandosuperiortransportation.pages.dev',
-  integrations: [sitemap(), tailwind()],
+  site: "https://new-custom-template.orlandosuperiortransportation.pages.dev",
+  integrations: [
+    sitemap(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+        debug: true,
+      },
+    }),
+  ],
   prefetch: {
-    defaultStrategy: 'hover',
-    prefetchAll: true
+    defaultStrategy: "hover",
+    prefetchAll: true,
   },
   output: "server",
   adapter: cloudflare({
-    mode: 'directory',
-    functionPerRoute: true
-  })
+    mode: "directory",
+    functionPerRoute: true,
+  }),
 });
