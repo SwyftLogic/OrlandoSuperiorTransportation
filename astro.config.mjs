d247074c9465c53b5a693279e1c4e2b696ca792f
hassaanfattani <hassaan_fattani@hotmail.com>
2024-02-13 13:00:37 +0500

    add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://new-custom-template.orlandosuperiortransportation.pages.dev',
@@ -9,5 +11,7 @@ export default defineConfig({
   prefetch: {
     defaultStrategy: 'hover',
     prefetchAll: true
-  }
+  },
+  output: "server",
+  adapter: cloudflare()
 });
