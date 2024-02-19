92e759e768003555f2bd0b13a9b0528629b30095
SwyftLogic <umair@swyftlogic.com>
2024-02-19 17:49:06 +0500

    configuration changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://new-custom-template.orlandosuperiortransportation.pages.dev",
+  site: "https://orlandosuperiortransportation.com",
   integrations: [
     sitemap(),
     tailwind(),
